d2d20ed062260f7f23f47916db9c9a83f554dd7d
TeshunXD <110263260+TeshunXD@users.noreply.github.com>
2023-12-25 15:41:20 -0600

    Update ultra.config.js

--- sodium-static/public/ultra/ultra.config.js
+++ sodium-static/public/ultra/ultra.config.js
@@ -13,7 +13,7 @@ self.__uv$config = {
     bare: [
         // Not Using a backend? Use this instead of the array below:
         //"https://server.flow-works.me/bare/"
-        "https://server.flow-works.me/bare/",
+      "https://phantomnetwork.cloud/bare/",
     ],
 
     /**
